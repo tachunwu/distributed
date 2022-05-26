86c6593f798bef4f0213fd23c290eea3108c787e
tachunwu <tachunwu.go@gmail.com>
2022-05-27 01:20:17 +0800

    test nats jetstream cluster

---
+++ cmd/jetstream_cluster/cluster_init.sh
@@ -0,0 +1,5 @@
+./../nats/nats-server -D -js -config cluster/n0.conf &
+./../nats/nats-server -D -js -config cluster/n1.conf &
+./../nats/nats-server -D -js -config cluster/n2.conf &
+./../nats/nats-server -D -js -config cluster/n3.conf &
+./../nats/nats-server -D -js -config cluster/n4.conf
